4f32feab57ebf74cb653260defb539a128798f1c
rw-nue <rw-nue@users.noreply.github.com>
2014-10-28 08:25:58 +0900

    add vundle to dotfiles

--- bin/init.sh
+++ bin/init.sh
@@ -4,6 +4,7 @@
 gitUserName="rw-nue";
 gitUserEmail="$gitUserName@users.noreply.github.com"
 DOT_FILES=( .vimrc .bashrc)
+DOT_DIRS=( .vim)
 
 # git config for dotfile directory
 cd $HOME/dotfiles
@@ -47,6 +48,22 @@ do
 				fi
 		else
 				echo "$file doesnt exist" ;
+		fi
+done
+for file in ${DOT_DIRS[@]}
+do
+		homeFile=$HOME/$file
+		dotFile=$dotfiles/$file
+		originalFile=$dotdotfiles/${file}.original
+		if [ ! -d "$dotFile" ]; then
+				echo "$dotFile doesnt exist"
+		elif [ -d "$homeFile" -a -L "$homeFile" ]; then
+				echo "has link already $homeFile" ;
+		else
+				echo "$originalFile backup "
+				mv $homeFile $originalFile
+				echo "ln -s $dotFile $homeFile"
+				ln -s $dotFile $homeFile
 		fi
 done
 
