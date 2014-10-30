5528991d99d1708da824d97b2eff1bdebd8c8d48
rw-nue <rw-nue@users.noreply.github.com>
2014-10-30 20:47:02 +0900

    added vundle update script

---
+++ bin/dots_vundle_update.sh
@@ -0,0 +1,11 @@
+
+
+
+# submodules
+dotfiles="$HOME/dotfiles"
+cd $dotfiles
+git submodule add -f https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
+git submodule update --init
+
+# Vundle install
+vim +PluginInstall! +qall
