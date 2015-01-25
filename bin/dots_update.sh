b935681c3572065757e32df8748557878290f1d7
rw-nue <rw-nue@users.noreply.github.com>
2015-01-25 13:06:29 +0900

    fixed updating script

--- bin/dots_update.sh
+++ bin/dots_update.sh
@@ -5,30 +5,33 @@ DOT_FILES=( .vimrc .bashrc)
 dotfiles="$HOME/dotfiles"
 dotdotfiles="$HOME/.dotfiles"
 
+echo "checking updating dotfiles ..."
 
 if [ ! -d "$dotdotfiles" ]; then
 		echo "$dotdotfiles has not created , please init first" ;
-else
-
-		for file in ${DOT_FILES[@]}
-		do
-				homeFile=$HOME/$file
-				dotFile=$dotfiles/$file
-				localFile=$dotdotfiles/${file}.local
-				originalFile=$dotdotfiles/${file}.original
-				mixedFile=$dotdotfiles/${file}
-
-				if [ ! -L "$homeFile" ]; then
-						echo "$homeFile is not Symbolic Link , please init first";
-				else
-						if [ ! -f "$dotFile" ]; then
-								echo "$dotFile doesnt exist" ;
-						else
-								cp $dotFile $mixedFile
-								if [ -a "$localFile" ]; then
-										cat $localFile >> $mixedFile
-								fi
-						fi
-				fi
-		done
+		exit 0 ;
 fi
+
+for file in ${DOT_FILES[@]}
+do
+		homeFile=$HOME/$file
+		dotFile=$dotfiles/$file
+		localFile=$dotdotfiles/${file}.local
+		originalFile=$dotdotfiles/${file}.original
+		mixedFile=$dotdotfiles/${file}
+
+		echo "checking updating $homeFile ...";
+
+		if [ ! -L "$homeFile" -o ! -f "$dotFile" ]; then
+				echo "$homeFile is not Symbolic Link nor $dotFile exits";
+				echo "passing this file ...";
+		else
+		echo "executing updating $homeFile ...";
+				cp $dotFile $mixedFile
+				if [ -f "$localFile" ]; then
+						cat "###### LOCAL FILE BELOW ######"
+						cat "# update $localFile and $dotfiles/bin/dot_update.sh to edit here#"
+						cat $localFile >> $mixedFile
+				fi
+		fi
+done
