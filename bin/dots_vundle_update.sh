6f18fa7c37471bc48b174897986c5f6c399453a5
rw-nue <rw-nue@users.noreply.github.com>
2015-03-06 19:45:03 +0900

    init with NeoBundle but Vundle

--- bin/dots_vundle_update.sh
+++ bin/dots_vundle_update.sh
@@ -4,8 +4,8 @@
 # submodules
 dotfiles="$HOME/dotfiles"
 cd $dotfiles
-git submodule add -f https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
+git submodule add -f https://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
 git submodule update --init
 
 # Vundle install
-vim +PluginInstall! +qall
+vim +NeoBundleUpdate +qall
